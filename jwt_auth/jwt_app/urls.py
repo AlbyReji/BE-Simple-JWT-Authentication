52fdab9ded0d2120a07f0c93bf9d202335357f08
AlbyReji <albymathew30@gmail.com>
2023-07-14 02:55:55 +0530

    Simple JWT Authentication

---
+++ jwt_auth/jwt_app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('booklist', views.PublicView.as_view(), name='booklist'),
+    path('booklist/<int:pk>/', views.BookView.as_view(), name='bookDetail'),
+
+    path('books', views.BookCreate.as_view(), name='booklist'),
+    path('books/<int:pk>/', views.BookDetail.as_view(), name='bookDetail'),
+
+    path('register/',views.Register.as_view(),name = "register"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ jwt_auth/jwt_app/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from .serializers import UserRegisterSerializer,BookSerializer
+from .models import Book
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView 
+from rest_framework import generics
+from rest_framework.generics import  RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .pagination import NumberPagination
+
+
+class PublicView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = NumberPagination
+
+class BookView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class Register(APIView):
+
+    def post(self, request, format=None):
+        serializer = UserRegisterSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = 'message:User created'
+            refresh = RefreshToken.for_user(account)
+        else:
+            data = serializer.errors
+        return Response(data)
+
+
+class BookCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(author=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(author=self.request.user)
+
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response({"message": "Book deleted successfully."})
+        except NotFound:
+            return Response({"message": "Book not found."})
+
